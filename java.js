da2f26d53d5a0c8ff2419e612298560c94db4d62
alumno <alumno@Instituto.local>
2025-03-18 09:53:45 -0300

    Update java.js

--- java.js
+++ java.js
@@ -1,3 +1,13 @@
 let añoNacimiento=2009
 alert('16')
 
+let nombreAlumno='Jorge'
+let edad=18
+
+if (edad>17){
+alert ('Podes ingresar a la GNU👍🔥 ')
+}
+    else
+{
+alert        ('No podes ingresar​ a la GNU😭​')
+}
